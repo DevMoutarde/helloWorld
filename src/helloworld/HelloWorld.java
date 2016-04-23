f97bfdc75c7f2f70a87f5bee172fcc63e44ffbb7
DevMoutarde <fourcault.gabin@gmail.com>
2016-04-23 15:52:27 +0200

    ajout d'un second print

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,6 +16,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("changement de programme");
+        System.out.println("changement de programme");
     }
     
 }
