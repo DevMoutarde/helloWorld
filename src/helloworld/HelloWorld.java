f28cf8c7e58ae852ecbc48795436b9adc349b448
DevMoutarde <fourcault.gabin@gmail.com>
2016-04-23 18:19:05 +0200

    changement du message en un message simple

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,7 +16,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
 
-        System.out.println("message initial");
+        System.out.println("message");
         
         
     }
